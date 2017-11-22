a7cfc4c560ecb9b036fdcb57c24f30adc38837dd
Xiao Jiang <showbufire@gmail.com>
2017-11-22 09:40:23 -0800

    [sampler] jittered

---
+++ RayUp/Sampler/Jittered.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace RayUp.Sampler
+{
+  public class Jittered : Sampler
+  {
+    public Jittered(int numSets, int numSamples): base(numSets, numSamples)
+    {
+    }
+
+    protected override void GenerateSamples()
+    {
+      Random rand = new Random();
+      for (int p = 0; p < numSets; p++)
+      {
+        for (int j = 0; j < n; j++)
+        {
+          for (int k = 0; k < n; k++)
+          {
+            Vector2 sp = new Vector2((k + rand.NextDouble()) / n, (j + rand.NextDouble()) / n);
+            samples.Add(sp);
+          }
+        }
+      }
+    }
+  }
+}
---
+++ RayUp/Sampler/Sampler.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RayUp.Sampler
+{
+  public abstract class Sampler
+  {
+    protected int numSamples;
+    protected int numSets;
+    protected List<Vector2> samples = new List<Vector2>();
+    protected int n;
+
+    private int count = 0;
+    private int jump = 0;
+    private Random rand;
+
+    public Sampler(int numSets, int numSamples)
+    {
+      this.numSets = numSets;
+      this.numSamples = numSamples;
+      n = (int)Math.Round(Math.Sqrt(numSamples));
+
+      GenerateSamples();
+    }
+
+    protected abstract void GenerateSamples();
+
+    public Vector2 SampleUnitSquare()
+    {
+      if (count == 0)
+      {
+        jump = rand.Next() % numSets;
+      }
+      Vector2 ret = samples[jump + count];
+      count += 1;
+      if (count == numSamples)
+      {
+        count = 0;
+      }
+      return ret;
+    }
+  }
+}
