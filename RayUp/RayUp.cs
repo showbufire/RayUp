5bed2adeadd207008dbcad145348b1355302a753
Xiao Jiang <showbufire@gmail.com>
2017-11-23 15:22:25 -0800

    measure and output time spent

--- RayUp/RayUp.cs
+++ RayUp/RayUp.cs
@@ -1,12 +1,19 @@
-﻿namespace RayUp
+﻿using System;
+using System.Diagnostics;
+
+namespace RayUp
 {
   class RayUp
   {
     static void Main(string[] args)
     {
+      Stopwatch stopwatch = new Stopwatch();
+      stopwatch.Start();
       World w = new World();
       w.Build();
       w.RenderScene();
+      stopwatch.Stop();
+      Console.WriteLine("Time taken: " + stopwatch.ElapsedMilliseconds);
     }
   }
 }
